fddb34c0ddc19a5651b627dd8502667de6807ffb
Habibun Noby <habibun.noby@gmail.com>
2022-03-11 15:39:35 +0600

    feat: add messenger

---
+++ .platform/hooks/postdeploy/01_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Star workers..."
+#su webapp -c "php bin/console messenger:consume async --memory-limit=128M -vv >> /var/www/sqs_queue.log 2>&1"
---
+++ .platform/hooks/prebuild/01_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Stop workers..."
+#su webapp -c "cd /var/www/html && php bin/console messenger:stop-workers >> /var/www/sqs_queue.log 2>&1"
